b4dc7d83760ef93490c4d7828c357fb4b0b0579e
MinaGhoreshi <ghoreishi45@gmail.com>
2024-05-26 17:32:50 +0330

    fix: debug table

--- packages/component/src/desktop/OrderProductsList.tsx
+++ packages/component/src/desktop/OrderProductsList.tsx
@@ -8,15 +8,68 @@ import useTranslation from "next-translate/useTranslation"
 import AddPriceModal from "./AddPriceModal"
 import CardContainer from "./CardContainer"
 
-type OrderProductsListProps = { isSellerAddPricePage?: boolean; data: any }
+type OrderProductsListProps = { hasExtraInfo?: boolean; data: any }
 
-function OrderProductsList({
-  isSellerAddPricePage,
-  data
-}: OrderProductsListProps) {
+const thClassName = "whitespace-nowrap border align-middle"
+const tdClassName = "border"
+const TablePriceHead = ({ isVardast }: { isVardast?: boolean }) => {
+  return (
+    <>
+      {isVardast && <th className={thClassName}>کمیسیون وردست (%)</th>}
+      <th className={thClassName}>قیمت واحد</th>
+      <th className={thClassName}>ارزش افزوده</th>
+      <th className={thClassName}>قیمت کل</th>
+    </>
+  )
+}
+
+function OrderProductsList({ hasExtraInfo, data }: OrderProductsListProps) {
   const { t } = useTranslation()
   const [open, setOpen] = useState<boolean>(false)
   const [productIdToEdit, setProdctIdToEdit] = useState<number>()
+
+  const tableHead = [
+    { colSpan: 1, display: true, element: "td", children: t("common:row") },
+    {
+      colSpan: 1,
+      display: true,
+      element: "td",
+      children: t("common:product_sku")
+    },
+    {
+      colSpan: 1,
+      display: true,
+      element: "td",
+      children: t("common:entity_name", { entity: t("common:product") })
+    },
+    { colSpan: 1, display: true, element: "td", children: t("common:brand") },
+    { colSpan: 1, display: true, element: "td", children: t("common:unit") },
+    { colSpan: 1, display: true, element: "td", children: t("common:value") },
+    {
+      colSpan: 1,
+      display: true,
+      element: "td",
+      children: t("common:attributes")
+    },
+    {
+      colSpan: 3,
+      display: true,
+      element: "div",
+      children: "قیمت خریدار (تومان)"
+    },
+    {
+      colSpan: 3,
+      display: hasExtraInfo,
+      element: "div",
+      children: "قیمت فروشنده (تومان)"
+    },
+    {
+      colSpan: 4,
+      display: hasExtraInfo,
+      element: "div",
+      children: "قیمت پیشنهادی وردست (تومان)"
+    }
+  ]
 
   const submit = (data: any) => {
     console.log(data)
@@ -33,114 +86,38 @@ function OrderProductsList({
       />
 
       <CardContainer title="لیست کالاها">
-        <div className={clsx(isSellerAddPricePage && "overflow-x-auto")}>
+        <div className="max-w-full overflow-x-auto">
           <table
             className={clsx(
-              "table min-w-full border-collapse border",
-              isSellerAddPricePage && "overflow-x-auto"
+              "table min-w-full border-collapse overflow-x-auto border"
             )}
           >
             <thead>
               <tr>
-                <th className="whitespace-nowrap border align-middle">
-                  {t("common:row")}
-                </th>
-                <th className="whitespace-nowrap border align-middle">
-                  {" "}
-                  {t("common:product_sku")}
-                </th>
-                <th className="whitespace-nowrap border align-middle">
-                  {t("common:entity_name", { entity: t("common:product") })}
-                </th>
-                <th className="whitespace-nowrap border align-middle">
-                  {t("common:brand")}
-                </th>
-                <th className="whitespace-nowrap border align-middle">
-                  {t("common:unit")}
-                </th>
-                <th className="whitespace-nowrap border align-middle">
-                  {t("common:value")}
-                </th>
-                <th className="whitespace-nowrap border align-middle">
-                  {t("common:attributes")}
-                </th>
-                <th
-                  className="flex w-full flex-col items-center whitespace-nowrap !px-0 !pb-0"
-                  // scope="col"
-                  // className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider text-gray-500"
-                >
-                  <div className="grid w-full grid-cols-3 border-b py-3">
-                    <div></div>
-                    <span className="flex justify-center">
-                      قیمت خریدار (تومان)
-                    </span>
-                    <div></div>
-                  </div>
-                  <div className="grid w-full grid-cols-3">
-                    <span className="flex justify-center border-x py-3">
-                      قیمت واحد
-                    </span>
-                    <span className="flex justify-center border-x py-3">
-                      مالیات ارزش افزوده
-                    </span>
-                    <span className="flex justify-center border-x py-3">
-                      قیمت کل
-                    </span>
-                  </div>
-                </th>
-                {isSellerAddPricePage && (
-                  <th className="whitespace-nowrap border align-middle">
-                    {t("common:operation")}
-                  </th>
+                {tableHead.map(
+                  (Thead) =>
+                    Thead.display && (
+                      <th
+                        colSpan={Thead.colSpan}
+                        rowSpan={Thead.element === "td" ? 2 : 1}
+                        className={clsx(
+                          thClassName,
+                          Thead.element === "div" && "!text-center"
+                        )}
+                      >
+                        {Thead.children}
+                      </th>
+                    )
                 )}
-                {/* {isSellerAddPricePage && (
-                <>
-                  <th
-                    className="flex w-full flex-col items-center whitespace-nowrap !px-0 !pb-0"
-                    // scope="col"
-                    // className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider text-gray-500"
-                  >
-                    <div className="grid w-full grid-cols-3 border-b py-3">
-                      <div></div>
-                      <span className="flex justify-center">قیمت خریدار</span>
-                      <div></div>
-                    </div>
-                    <div className="grid w-full grid-cols-3">
-                      <span className="flex justify-center border-x py-3">
-                        قیممت واحد
-                      </span>
-                      <span className="flex justify-center border-x py-3">
-                        مالیات ارزش افزوده
-                      </span>
-                      <span className="flex justify-center border-x py-3">
-                        قیمت کل
-                      </span>
-                    </div>
-                  </th>
-                  <th
-                    className="flex w-full flex-col items-center whitespace-nowrap !px-0 !pb-0"
-                    // scope="col"
-                    // className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider text-gray-500"
-                  >
-                    <div className="grid w-full grid-cols-3 border-b py-3">
-                      <div></div>
-                      <span className="flex justify-center">قیمت خریدار</span>
-                      <div></div>
-                    </div>
-                    <div className="grid w-full grid-cols-3">
-                      <span className="flex justify-center border-x py-3">
-                        قیممت واحد
-                      </span>
-                      <span className="flex justify-center border-x py-3">
-                        مالیات ارزش افزوده
-                      </span>
-                      <span className="flex justify-center border-x py-3">
-                        قیمت کل
-                      </span>
-                    </div>
-                  </th>
-                </>
-              )} */}
+              </tr>
+              <tr>
+                <TablePriceHead />
+                {hasExtraInfo && (
+                  <>
+                    <TablePriceHead />
+                    <TablePriceHead isVardast={true} />
+                  </>
+                )}
               </tr>
             </thead>
 
@@ -149,29 +126,29 @@ function OrderProductsList({
                 (order, index) =>
                   order && (
                     <tr key={order.id}>
-                      <td className="w-4 border">
+                      <td className={tdClassName}>
                         <span>{digitsEnToFa(index + 1)}</span>
                       </td>
-                      <td className="border">
+                      <td className={tdClassName}>
                         <span className="font-medium text-alpha-800">
                           {order.product_sku}
                         </span>
                       </td>
-                      <td className="border">
+                      <td className={tdClassName}>
                         <span className="font-medium text-alpha-800">
                           {order.productName}
                         </span>
                       </td>
-                      <td className="border">
+                      <td className={tdClassName}>
                         <span className="font-medium text-alpha-800">
                           {order.brand}
                         </span>
                       </td>
 
-                      <td className="border">{digitsEnToFa(1236)}</td>
+                      <td className={tdClassName}>{digitsEnToFa(1236)}</td>
 
-                      <td className="border">{digitsEnToFa(85495)}</td>
-                      <td className="border">
+                      <td className={tdClassName}>{digitsEnToFa(85495)}</td>
+                      <td className={tdClassName}>
                         <div className="flex gap-1">
                           {order.attributes.map((attribute) => (
                             <span className="tag tag-sm tag-gray">
@@ -180,36 +157,46 @@ function OrderProductsList({
                           ))}
                         </div>
                       </td>
-                      <td className="grid grid-cols-3  !p-0">
-                        <span className="flex justify-center border-x px-4 py-3">
-                          {" "}
-                          {digitsEnToFa(
-                            addCommas(order.purchaserPrice.basePrice)
-                          )}
-                        </span>
-                        <span className="flex justify-center border-x px-4 py-3">
-                          {" "}
-                          {digitsEnToFa(addCommas(order.purchaserPrice.tax))}
-                        </span>
-                        <span className="flex justify-center border-x px-4 py-3">
-                          {" "}
-                          {digitsEnToFa(addCommas(order.purchaserPrice.total))}
-                        </span>
+                      <td className="border-x px-4 py-3">
+                        {digitsEnToFa(
+                          addCommas(order.purchaserPrice.basePrice)
+                        )}
                       </td>
-                      {isSellerAddPricePage && (
-                        <td className="border">
-                          <span
-                            onClick={() => {
-                              setProdctIdToEdit(order.id)
-                              setOpen(true)
-                            }}
-                            className="tag cursor-pointer text-blue-500"
-                          >
-                            {t("common:add_entity", {
-                              entity: t("common:price")
-                            })}
-                          </span>
-                        </td>
+                      <td className="border-x px-4 py-3">
+                        {digitsEnToFa(addCommas(order.purchaserPrice.tax))}
+                      </td>
+                      <td className="border-x px-4 py-3">
+                        {digitsEnToFa(addCommas(order.purchaserPrice.total))}
+                      </td>
+                      {hasExtraInfo && (
+                        <>
+                          <td className="border-x px-4 py-3">
+                            {digitsEnToFa(
+                              addCommas(order.purchaserPrice.basePrice)
+                            )}
+                          </td>
+                          <td className="border-x px-4 py-3">
+                            {digitsEnToFa(addCommas(order.purchaserPrice.tax))}
+                          </td>
+                          <td className="border-x px-4 py-3">
+                            {digitsEnToFa(
+                              addCommas(order.purchaserPrice.total)
+                            )}
+                          </td>
+                          <td className="border-x px-4 py-3">
+                            {digitsEnToFa(
+                              addCommas(order.purchaserPrice.basePrice)
+                            )}
+                          </td>
+                          <td className="border-x px-4 py-3">
+                            {digitsEnToFa(addCommas(order.purchaserPrice.tax))}
+                          </td>
+                          <td className="border-x px-4 py-3">
+                            {digitsEnToFa(
+                              addCommas(order.purchaserPrice.total)
+                            )}
+                          </td>
+                        </>
                       )}
                     </tr>
                   )
