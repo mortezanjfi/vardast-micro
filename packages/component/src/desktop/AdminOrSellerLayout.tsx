055130b3eff7ed1b1d77eb6eb778936a6c9d9fef
MinaGhoreshi <ghoreishi45@gmail.com>
2024-05-26 17:32:18 +0330

    fix: fix style

--- packages/component/src/desktop/AdminOrSellerLayout.tsx
+++ packages/component/src/desktop/AdminOrSellerLayout.tsx
@@ -20,33 +20,31 @@ const AdminOrSellerLayoutComponent = ({
   const [sidebarOpen, setSidebarOpen] = useState<boolean>(false)
 
   return (
-    <div className="md:app w-full bg-alpha-50">
-      <div className=" hide-scrollbar h-full w-full flex-col overflow-y-scroll  pb-8 lg:container md:overflow-y-auto">
-        <div className="app-inner gap-3 pb-5 pt">
-          <Sidebar
-            open={sidebarOpen}
-            onOpenChanged={setSidebarOpen}
-            menus={menu}
-            isAdmin={true}
-          />
-          <div className="flex w-full flex-col">
-            <div className="flex items-center gap-2 pr-6">
-              <Button
-                onClick={() => setSidebarOpen(true)}
-                variant="ghost"
-                iconOnly
-                className="lg:hidden"
-              >
-                <LucideMenu className="icon" />
-              </Button>
-              <div className="flex-1 overflow-y-auto">
-                <Suspense>
-                  <Breadcrumb dynamic />
-                </Suspense>
-              </div>
-            </div>{" "}
-            {children}
-          </div>
+    <div className=" hide-scrollbar h-full w-full flex-col overflow-y-scroll bg-alpha-50  pb-8 lg:container md:overflow-y-auto">
+      <div className="app-inner gap-3 pb-5 pt">
+        <Sidebar
+          open={sidebarOpen}
+          onOpenChanged={setSidebarOpen}
+          menus={menu}
+          isAdmin={true}
+        />
+        <div className="flex h-full w-full flex-col">
+          <div className="flex items-center gap-2 pr-6">
+            <Button
+              onClick={() => setSidebarOpen(true)}
+              variant="ghost"
+              iconOnly
+              className="lg:hidden"
+            >
+              <LucideMenu className="icon" />
+            </Button>
+            <div className="flex-1 overflow-y-auto">
+              <Suspense>
+                <Breadcrumb dynamic />
+              </Suspense>
+            </div>
+          </div>{" "}
+          {children}
         </div>
       </div>
     </div>
