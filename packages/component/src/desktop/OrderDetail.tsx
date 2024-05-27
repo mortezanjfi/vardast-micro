b42df1e02adb99bd4cdbef3e71d23b067a2e6770
MinaGhoreshi <ghoreishi45@gmail.com>
2024-05-27 16:59:37 +0330

    feat: add data in order info card

--- packages/component/src/desktop/OrderDetail.tsx
+++ packages/component/src/desktop/OrderDetail.tsx
@@ -1,4 +1,8 @@
+"use client"
+
 import { ReactNode } from "react"
+import { UseQueryResult } from "@tanstack/react-query"
+import { FindPreOrderByIdQuery } from "@vardast/graphql/generated"
 
 import OrderInfoCard from "./OrderInfoCard"
 import OrderProductsList from "./OrderProductsList"
@@ -11,27 +15,43 @@ export enum OrderDetailPageType {
 }
 
 type Props = {
+  uuid?: string
   isAdmin?: boolean
   type: OrderDetailPageType
   AddOfferChildren?: ReactNode
   SellerChildren?: ReactNode
   Adminchildren?: ReactNode
-  data: any
+  findPreOrderByIdQuery: UseQueryResult<FindPreOrderByIdQuery, unknown>
   SellerAddOfferPriceChildren?: ReactNode
 }
-
+const fakeData = [
+  {
+    id: 3,
+    product_sku: "Innovative AI Development",
+    productName: "test",
+    brand: "test brand",
+    unit: "60",
+    value: 4,
+    attributes: ["test", "test2"],
+    purchaserPrice: { basePrice: 300, tax: 40, total: 340 }
+  }
+]
 const OrderDetail: React.FC<Props> = ({
+  uuid,
   SellerAddOfferPriceChildren,
   type,
   Adminchildren,
   AddOfferChildren,
   SellerChildren,
-  data
+  findPreOrderByIdQuery
 }) => {
   return (
     <div className="flex w-full flex-col gap-9">
-      <OrderInfoCard />
-      <OrderProductsList data={data} />
+      <OrderInfoCard
+        findPreOrderByIdQuery={findPreOrderByIdQuery}
+        uuid={uuid}
+      />
+      <OrderProductsList data={fakeData} />
       {type === OrderDetailPageType.SELLER_ORDERS_DETAIL && SellerChildren}
       {type === OrderDetailPageType.ADMIN_ORDER_DETAIL_PAGE && Adminchildren}
       {type === OrderDetailPageType.SELLER_ADD_OFFER_PRICE_PAGE &&
--- packages/component/src/desktop/OrderInfoCard.tsx
+++ packages/component/src/desktop/OrderInfoCard.tsx
@@ -1,31 +1,56 @@
+"use client"
+
 import { digitsEnToFa } from "@persian-tools/persian-tools"
+import { UseQueryResult } from "@tanstack/react-query"
+import {
+  FindPreOrderByIdQuery,
+  PaymentMethodEnum,
+  PreOrderStates
+} from "@vardast/graphql/generated"
 import clsx from "clsx"
 import useTranslation from "next-translate/useTranslation"
 
 import Card from "../Card"
 import { DetailsWithTitle } from "./DetailsWithTitle"
 
-type OrderInfoCardProps = { uuid?: string }
+type OrderInfoCardProps = {
+  findPreOrderByIdQuery: UseQueryResult<FindPreOrderByIdQuery, unknown>
+  uuid?: string
+}
 
-const OrderInfoCard = async ({ uuid }: OrderInfoCardProps) => {
+export const PaymentMethodEnumFa = {
+  [PaymentMethodEnum.Cash]: {
+    className: "",
+    name_fa: "نقدی"
+  },
+  [PaymentMethodEnum.Credit]: {
+    className: "",
+    name_fa: "غیر نقدی"
+  }
+}
+
+export const PreOrderStatesFa = {
+  [PreOrderStates.Created]: {
+    className: "tag-secondary",
+    name_fa: "ایجاد شده"
+  },
+  [PreOrderStates.PendingInfo]: {
+    className: "tag-warning",
+    name_fa: "در انتظار تایید اطلاعات"
+  },
+  [PreOrderStates.PendingLine]: {
+    className: "tag-warning",
+    name_fa: "در انتظار افزودن کالا"
+  },
+  [PreOrderStates.Verified]: { className: "tag-success", name_fa: "تایید شده" }
+}
+
+const OrderInfoCard = ({ findPreOrderByIdQuery, uuid }: OrderInfoCardProps) => {
   const { t } = useTranslation()
 
-  const orderInfo = {
-    purchaserName: "Fake Order 1",
-    purchaserNumber: "Pending",
-    payMethod: "Cash",
-    projectCode: 123,
-    name: "test2",
-    createdAt: new Date(),
-    expiresAt: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
-    projectAddress: "address should be displayed here",
-    transfereeName: "Ms. Test",
-    transfereeNumber: 9122222222,
-    shippingAddress: "address should be displayed here",
-    payDescription: "description displayed here",
-    orderDescription: "order description should e here",
-    status: "test"
-  }
+  const orderInfo = findPreOrderByIdQuery?.data?.findPreOrderById
+
+  console.log({ orderInfo })
 
   return (
     <Card
@@ -37,31 +62,37 @@ const OrderInfoCard = async ({ uuid }: OrderInfoCardProps) => {
         <div className="flex gap-5">
           <div className="tag flex w-fit gap-2 border border-alpha-400 bg-alpha-50 px-4 py-1">
             <span>{t("entity_code", { entity: t("common:project") })}</span>
-            <span>{orderInfo.projectCode}</span>
+            <span>{digitsEnToFa(uuid)}</span>
           </div>
-          <div className="tag flex w-fit gap-2 border border-alpha-400 bg-alpha-50 px-4 py-1">
-            <span>{orderInfo.status}</span>
+          <div
+            className={clsx(
+              "tag",
+              PreOrderStatesFa[orderInfo?.status]?.className
+            )}
+          >
+            {/* <Dot /> */}
+            <span>{PreOrderStatesFa[orderInfo?.status]?.name_fa}</span>
           </div>
         </div>
-        <div className={clsx("grid grid-cols-2 2xl:grid-cols-3")}>
+        <div className={"grid grid-cols-2 2xl:grid-cols-3"}>
           {/* نام خریدار */}
           <div className="flex flex-col">
             <DetailsWithTitle
               textCustomStyle="whitespace-nowrap line-clamp-1"
               title={t("common:entity_name", { entity: t("common:purchaser") })}
-              text={orderInfo.purchaserName}
+              text={orderInfo?.user?.fullName}
             />
             {/* شماره تماس خریدار */}
             <DetailsWithTitle
               textCustomStyle="whitespace-nowrap line-clamp-1"
               title={t("common:purchaser-number")}
-              text={orderInfo.purchaserNumber}
+              text={orderInfo?.user?.cellphone}
             />
             {/* نام پروژه */}
             <DetailsWithTitle
               textCustomStyle="whitespace-nowrap line-clamp-1"
               title={t("common:entity_name", { entity: t("common:project") })}
-              text={orderInfo.name}
+              text={orderInfo?.project?.name}
             />
 
             <>
@@ -69,38 +100,38 @@ const OrderInfoCard = async ({ uuid }: OrderInfoCardProps) => {
               <DetailsWithTitle
                 textCustomStyle="whitespace-nowrap line-clamp-1"
                 title={t("common:project-address")}
-                text={orderInfo.projectAddress}
+                text={orderInfo?.project?.address[0]?.address?.address}
               />
               {/*تحویل گیرنده*/}
               <DetailsWithTitle
                 textCustomStyle="whitespace-nowrap line-clamp-1"
                 title={t("common:transferee")}
-                text={orderInfo.transfereeName}
+                text={"-"}
               />
               {/*شماره تماس تحویل گیرنده */}
               <DetailsWithTitle
                 textCustomStyle="whitespace-nowrap line-clamp-1"
                 title={t("common:transferee-number")}
-                text={orderInfo.transfereeNumber}
+                text={"-"}
               />{" "}
             </>
           </div>
           {/*آدرس محل تحویل*/}
-          <div className={clsx("flex flex-col 2xl:col-span-2")}>
+          <div className={"flex flex-col 2xl:col-span-2"}>
             <>
               <DetailsWithTitle
                 textCustomStyle="whitespace-nowrap line-clamp-1"
                 title={t("common:shipping-address")}
-                text={orderInfo.shippingAddress}
+                text={orderInfo?.shipping_address}
               />{" "}
               {/* زمان ثبت سفارش*/}
               <DetailsWithTitle
                 textCustomStyle="whitespace-nowrap line-clamp-1"
                 title={t("common:submition-time")}
                 text={
-                  orderInfo.createdAt
+                  orderInfo?.request_date
                     ? digitsEnToFa(
-                        new Date(orderInfo.createdAt).toLocaleDateString(
+                        new Date(orderInfo?.request_date).toLocaleDateString(
                           "fa-IR",
                           {
                             year: "numeric",
@@ -115,24 +146,23 @@ const OrderInfoCard = async ({ uuid }: OrderInfoCardProps) => {
             </>
             {/*روش پرداخت */}
             <DetailsWithTitle
-              textCustomStyle="whitespace-nowrap line-clamp-1"
               title={t("common:pay-method")}
-              text={orderInfo.payMethod}
+              text={PaymentMethodEnumFa[orderInfo?.payment_methods]?.name_fa}
             />
             {/* توضیحات روش پرداخت*/}
             <DetailsWithTitle
               textCustomStyle="whitespace-nowrap line-clamp-1"
               title={t("common:pay-method-description")}
-              text={orderInfo.payDescription}
+              text={"-"}
             />
             {/*زمان اعتبار سفارش*/}
             <DetailsWithTitle
               textCustomStyle="whitespace-nowrap line-clamp-1"
               title={t("common:order-expire-time")}
               text={
-                orderInfo.expiresAt
+                orderInfo?.expire_date
                   ? digitsEnToFa(
-                      new Date(orderInfo.expiresAt).toLocaleDateString(
+                      new Date(orderInfo?.expire_date).toLocaleDateString(
                         "fa-IR",
                         {
                           year: "numeric",
@@ -148,7 +178,7 @@ const OrderInfoCard = async ({ uuid }: OrderInfoCardProps) => {
             <DetailsWithTitle
               textCustomStyle="whitespace-nowrap line-clamp-1"
               title="توضیحات سفارش"
-              text={orderInfo.orderDescription}
+              text={orderInfo?.descriptions}
             />
           </div>
         </div>
