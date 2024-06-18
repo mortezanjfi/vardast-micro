a7ed1b4f13dabec462f1623bb0ac3334899cc4ba
MinaGhoreshi <ghoreishi45@gmail.com>
2024-06-18 16:47:59 +0330

    feat: add wallet info to legal users table

--- packages/component/src/legal/AllLegalUsers.tsx
+++ packages/component/src/legal/AllLegalUsers.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react"
 import { useRouter } from "next/navigation"
-import { digitsEnToFa } from "@persian-tools/persian-tools"
+import { addCommas, digitsEnToFa } from "@persian-tools/persian-tools"
 import { useGetAllLegalUsersQuery } from "@vardast/graphql/generated"
 import graphqlRequestClientWithToken from "@vardast/query/queryClients/graphqlRequestClientWithToken"
 import useTranslation from "next-translate/useTranslation"
@@ -12,6 +12,8 @@ import Pagination from "../Pagination"
 import AddLegalUserModal from "./AddLegalUserModal"
 
 type Props = {}
+
+const thClass = "border"
 
 export default (props: Props) => {
   const { t } = useTranslation()
@@ -48,18 +50,21 @@ export default (props: Props) => {
         <table className="table-hover table">
           <thead>
             <tr>
-              <th>{t("common:row")}</th>
-              <th>
+              <th className={thClass}>{t("common:row")}</th>
+              <th className={thClass}>
                 {t("common:entity_name", { entity: t("common:company") })}
               </th>
-              <th>
+              <th className={thClass}>
                 {t("common:entity_uuid", { entity: t("common:national") })}
               </th>
-              <th>
+              <th className={thClass}>
                 {t("common:entity_name", { entity: t("common:manager") })}
               </th>
-              <th>{t("common:entity_count", { entity: t("common:order") })}</th>
-              <th>{t("common:status")}</th>
+              <th className={thClass}>{t("common:wallet")} (تومان)</th>
+              <th className={thClass}>
+                {t("common:entity_count", { entity: t("common:order") })}
+              </th>
+              <th className={thClass}>{t("common:status")}</th>
               {/* <th>{t("common:operation")}</th> */}
             </tr>
           </thead>
@@ -72,11 +77,14 @@ export default (props: Props) => {
                     <td className="w-4">
                       <span>{digitsEnToFa(index + 1)}</span>
                     </td>
-                    <td>{user?.name_company}</td>
-                    <td>{user?.national_id}</td>
-                    <td>{user?.createdBy?.fullName}</td>
-                    <td>--</td>
-                    <td>--</td>
+                    <td className="border">{user?.name_company}</td>
+                    <td className="border">{user?.national_id}</td>
+                    <td className="border">{user?.createdBy?.fullName}</td>
+                    <td className="border">
+                      {digitsEnToFa(addCommas(user?.wallet))}
+                    </td>
+                    <td className="border">--</td>
+                    <td className="border">--</td>
                   </tr>
                 )
             )}
