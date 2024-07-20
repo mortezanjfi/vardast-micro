f62ede161a38d46a0ffa62474f7c0ae140478712
mina ghoreshi <goreishi45@gmail.com>
2024-07-20 13:33:18 +0330

    feat: add create slider mutation and dropzones

--- apps/vardast-admin/src/app/(admin)/app-management/main/components/NewSliderForm.tsx
+++ apps/vardast-admin/src/app/(admin)/app-management/main/components/NewSliderForm.tsx
@@ -2,10 +2,16 @@
 
 import { useState } from "react"
 import { zodResolver } from "@hookform/resolvers/zod"
+import { useQueryClient } from "@tanstack/react-query"
 import CardContainer from "@vardast/component/desktop/CardContainer"
 import Dropzone from "@vardast/component/Dropzone"
-import { ThreeBannerStatuses } from "@vardast/graphql/generated"
+import {
+  ThreeBannerStatuses,
+  useCreateBannerMutation
+} from "@vardast/graphql/generated"
+import { useToast } from "@vardast/hook/use-toast"
 import { uploadPaths } from "@vardast/lib/uploadPaths"
+import graphqlRequestClientWithToken from "@vardast/query/queryClients/graphqlRequestClientWithToken"
 import { mergeClasses } from "@vardast/tailwind-config/mergeClasses"
 import { Button } from "@vardast/ui/button"
 import {
@@ -24,6 +30,7 @@ import {
 } from "@vardast/ui/form"
 import { Input } from "@vardast/ui/input"
 import { Popover, PopoverContent, PopoverTrigger } from "@vardast/ui/popover"
+import { ClientError } from "graphql-request"
 import { LucideCheck, LucideChevronsUpDown } from "lucide-react"
 import useTranslation from "next-translate/useTranslation"
 import { useForm } from "react-hook-form"
@@ -57,19 +64,54 @@ type CreateSliderType = TypeOf<typeof SliderSchema>
 
 function NewSliderForm({}: Props) {
   const { t } = useTranslation()
+  const [errors, setErrors] = useState<ClientError>()
+  const queryClient = useQueryClient()
+  const { toast } = useToast()
+
   const [statusDialog, setStatusDialog] = useState<boolean>(false)
-  const [smallUuid, setSmallUuid] = useState<string>("")
-  const [mediumUuid, setMediumUuid] = useState<string>("")
-  const [largeUuid, setLargeUuid] = useState<string>("")
-  const [xlargeUuid, setXlargeUuid] = useState<string>("")
+  // const [smallUuid, setSmallUuid] = useState<string>("")
+  // const [mediumUuid, setMediumUuid] = useState<string>("")
+  // const [largeUuid, setLargeUuid] = useState<string>("")
+  // const [xlargeUuid, setXlargeUuid] = useState<string>("")
 
   const form = useForm<CreateSliderType>({
     resolver: zodResolver(SliderSchema),
     defaultValues: {}
   })
+
+  const createSliderMutation = useCreateBannerMutation(
+    graphqlRequestClientWithToken,
+    {
+      onError: (errors: ClientError) => {
+        setErrors(errors)
+      },
+      onSuccess: () => {
+        queryClient.invalidateQueries({
+          queryKey: ["GetSlidersQuery"]
+        })
+        toast({
+          description: t("common:entity_added_successfully", {
+            entity: t("common:slider")
+          }),
+          duration: 2000,
+          variant: "success"
+        })
+        // router.push("/brands")
+      }
+    }
+  )
+
   const handleSubmit = (data: CreateSliderType) => {
-    form.setValue("smallUuid", smallUuid)
-    console.log(data)
+    createSliderMutation.mutate({
+      createBannerInput: {
+        name: data.name,
+        link_url: data.link,
+        large_uuid: form.getValues("largeUuid"),
+        medium_uuid: form.getValues("mediumUuid"),
+        small_uuid: form.getValues("smallUuid"),
+        xlarge_uuid: form.getValues("xlargeUuid")
+      }
+    })
   }
 
   return (
@@ -105,6 +147,7 @@ function NewSliderForm({}: Props) {
             />
             <FormField
               control={form.control}
+              disabled
               name="sort"
               render={({ field }) => (
                 <FormItem>
@@ -183,59 +226,66 @@ function NewSliderForm({}: Props) {
               <li>حجم آن نباید بیشتر از 50 مگابایت باشد.</li>
               <li>امکان آپلود چند فایل به صورت همزمان وجود ندارد.</li>
             </ul>
-            <div className="col-span-2">
+            <div className="col-span-2 flex flex-col gap-2">
+              <span>{t("common:entity_size", { entity: "Small" })}</span>
               <Dropzone
+                maxFiles={1}
                 withHeight={false}
-                uploadPath={uploadPaths.productImages}
+                uploadPath={uploadPaths.brandCatalog}
                 onAddition={(file) => {
-                  console.log("add")
-                  setSmallUuid(file.uuid)
+                  form.setValue("smallUuid", file.uuid)
                 }}
                 onDelete={(file) => {
-                  console.log("delete")
-                  setSmallUuid("")
+                  // setSmallUuid("")
                 }}
               />
             </div>
-            <div className="col-span-2">
+            <div className="col-span-2 flex flex-col gap-2">
+              <span>{t("common:entity_size", { entity: "Medium" })}</span>
               <Dropzone
                 withHeight={false}
-                uploadPath={uploadPaths.productImages}
+                uploadPath={uploadPaths.brandCatalog}
                 onAddition={(file) => {
                   console.log("add")
-                  setSmallUuid(file.uuid)
+                  // setMediumUuid(file.uuid)
+                  form.setValue("mediumUuid", file.uuid)
                 }}
                 onDelete={(file) => {
                   console.log("delete")
-                  setMediumUuid("")
+                  // setMediumUuid("")
                 }}
               />
             </div>
-            <div className="col-span-2">
+            <div className="col-span-2 flex flex-col gap-2">
+              <span>{t("common:entity_size", { entity: "Large" })}</span>{" "}
               <Dropzone
                 withHeight={false}
-                uploadPath={uploadPaths.productImages}
+                uploadPath={uploadPaths.brandCatalog}
                 onAddition={(file) => {
                   console.log("add")
-                  setLargeUuid(file.uuid)
+                  form.setValue("largeUuid", file.uuid)
+
+                  // setLargeUuid(file.uuid)
                 }}
                 onDelete={(file) => {
                   console.log("delete")
-                  setLargeUuid("")
+                  // setLargeUuid("")
                 }}
               />
             </div>
-            <div className="col-span-2">
+            <div className="col-span-2 flex flex-col gap-2">
+              <span>{t("common:entity_size", { entity: "Xlarge" })}</span>{" "}
               <Dropzone
                 withHeight={false}
-                uploadPath={uploadPaths.productImages}
+                uploadPath={uploadPaths.brandCatalog}
                 onAddition={(file) => {
                   console.log("add")
-                  setXlargeUuid(file.uuid)
+                  form.setValue("xlargeUuid", file.uuid)
+                  // setXlargeUuid(file.uuid)
                 }}
                 onDelete={(file) => {
                   console.log("delete")
-                  setXlargeUuid("")
+                  // setXlargeUuid("")
                 }}
               />
             </div>
