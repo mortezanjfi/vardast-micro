6e01068e73c18dca5242396a02b22943cd624084
mina ghoreshi <goreishi45@gmail.com>
2024-07-21 16:40:23 +0330

    feat: implement remove banner

--- apps/vardast-admin/src/app/(admin)/app-management/main/components/SlidersPage.tsx
+++ apps/vardast-admin/src/app/(admin)/app-management/main/components/SlidersPage.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useMemo } from "react"
+import { useMemo, useState } from "react"
 import { useRouter } from "next/navigation"
 import { digitsEnToFa } from "@persian-tools/persian-tools"
 import CardContainer from "@vardast/component/desktop/CardContainer"
@@ -9,6 +9,7 @@ import Loading from "@vardast/component/Loading"
 import LoadingFailed from "@vardast/component/LoadingFailed"
 import NoResult from "@vardast/component/NoResult"
 import {
+  Banner,
   ThreeBannerStatuses,
   useGetSlidersQuery
 } from "@vardast/graphql/generated"
@@ -17,6 +18,8 @@ import { ApiCallStatusEnum } from "@vardast/type/Enums"
 import { getContentByApiStatus } from "@vardast/util/GetContentByApiStatus"
 import clsx from "clsx"
 import useTranslation from "next-translate/useTranslation"
+
+import SliderDeleteModal from "@/app/(admin)/app-management/main/components/SliderDeleteModal"
 
 const renderedListStatus = {
   [ApiCallStatusEnum.LOADING]: <Loading />,
@@ -42,6 +45,9 @@ export const statusSliderFa = {
 function SlidersPage({}: Props) {
   const { t } = useTranslation()
   const router = useRouter()
+  const [deleteModalOpen, setDeleteModalOpen] = useState<boolean>(false)
+  const [sliderToDelete, setSliderToDelete] = useState<Banner>()
+
   const sliders = useGetSlidersQuery(graphqlRequestClientWithToken, {
     IndexBannerInput: {}
   })
@@ -56,119 +62,136 @@ function SlidersPage({}: Props) {
     router.push("/app-management/main/new-slider")
   }
   return (
-    <div className="flex flex-col gap-7">
-      <CardContainer
-        button={{
-          disabled: sliders.isLoading || sliders.isLoading,
-          loading: sliders.isLoading,
-          onClick: addSlider,
-          text: "افزودن اسلایدر",
-          variant: "primary"
-        }}
-        title="لیست‌ اسلایدرها"
-      >
-        {renderedListStatus[getContentByApiStatus(sliders, !!ordersLength)] || (
-          <>
-            <div className="overflow-x-scroll">
-              <table className="table-hover table">
-                <thead>
-                  <tr>
-                    <th>{t("common:row")}</th>
-                    <th>{t("common:title")}</th>
-                    <th className="border">{t("common:link")}</th>
+    <>
+      <SliderDeleteModal
+        open={deleteModalOpen}
+        sliderToDelete={sliderToDelete as Banner}
+        onOpenChange={setDeleteModalOpen}
+      />
+      <div className="flex flex-col gap-7">
+        <CardContainer
+          button={{
+            disabled: sliders.isLoading || sliders.isLoading,
+            loading: sliders.isLoading,
+            onClick: addSlider,
+            text: "افزودن اسلایدر",
+            variant: "primary"
+          }}
+          title="لیست‌ اسلایدرها"
+        >
+          {renderedListStatus[
+            getContentByApiStatus(sliders, !!ordersLength)
+          ] || (
+            <>
+              <div className="overflow-x-scroll">
+                <table className="table-hover table">
+                  <thead>
+                    <tr>
+                      <th>{t("common:row")}</th>
+                      <th>{t("common:title")}</th>
+                      <th className="border">{t("common:link")}</th>
 
-                    <th>
-                      {t("common:entity_size", {
-                        entity: t("common:image")
-                      })}
-                    </th>
-                    {/* <th>{t("common:display_sort")}</th> */}
-                    <th>{t("common:status")}</th>
-                    <th>{t("common:operation")}</th>
-                  </tr>
-                </thead>
+                      <th>
+                        {t("common:entity_size", {
+                          entity: t("common:image")
+                        })}
+                      </th>
+                      {/* <th>{t("common:display_sort")}</th> */}
+                      <th>{t("common:status")}</th>
+                      <th>{t("common:operation")}</th>
+                    </tr>
+                  </thead>
 
-                <tbody className="border-collapse border">
-                  {sliders.data.getBanners.map(
-                    (slider, index) =>
-                      slider && (
-                        <tr className=" hover:bg-alpha-50" key={slider?.id}>
-                          <td className="w-4 border">
-                            <span>{digitsEnToFa(index + 1)}</span>
-                          </td>
-                          <td>{slider?.name || "-"}</td>
-                          <td>{slider?.url || "-"}</td>
-                          <td className="flex gap-1">
-                            <span
-                              className={clsx(
-                                "tag ",
-                                slider.small.id ? "tag-success" : "tag-danger"
-                              )}
-                            >
-                              SM
-                            </span>
-                            <span
-                              className={clsx(
-                                "tag ",
-                                slider.medium.id ? "tag-success" : "tag-danger"
-                              )}
-                            >
-                              MD
-                            </span>
-                            <span
-                              className={clsx(
-                                "tag",
-                                slider.large.id ? "tag-success" : "tag-danger"
-                              )}
-                            >
-                              LG
-                            </span>
-                            <span
-                              className={clsx(
-                                "tag ",
-                                slider.xlarge.id ? "tag-success" : "tag-danger"
-                              )}
-                            >
-                              XL
-                            </span>
-                          </td>
-                          <td>{statusSliderFa[slider?.status]?.name_fa}</td>
-                          <td>
-                            <Link
-                              target="_blank"
-                              href={`/app-management/main/${slider?.id}`}
-                            >
-                              <span className="tag cursor-pointer text-blue-500">
-                                {t("common:edit")}
+                  <tbody className="border-collapse border">
+                    {sliders.data.getBanners.map(
+                      (slider, index) =>
+                        slider && (
+                          <tr className=" hover:bg-alpha-50" key={slider?.id}>
+                            <td className="w-4 border">
+                              <span>{digitsEnToFa(index + 1)}</span>
+                            </td>
+                            <td>{slider?.name || "-"}</td>
+                            <td>{slider?.url || "-"}</td>
+                            <td className="flex gap-1">
+                              <span
+                                className={clsx(
+                                  "tag ",
+                                  slider.small.id ? "tag-success" : "tag-danger"
+                                )}
+                              >
+                                SM
                               </span>
-                            </Link>
-                            {/* /
-                            <span
-                              className="tag cursor-pointer text-error"
-                              onClick={(e) => {
-                                e.stopPropagation()
-                              }}
-                            >
-                              {t("common:delete")}
-                            </span> */}
-                          </td>
-                        </tr>
-                      )
-                  )}
-                </tbody>
-              </table>
-            </div>
-            {/* <Pagination
+                              <span
+                                className={clsx(
+                                  "tag ",
+                                  slider.medium.id
+                                    ? "tag-success"
+                                    : "tag-danger"
+                                )}
+                              >
+                                MD
+                              </span>
+                              <span
+                                className={clsx(
+                                  "tag",
+                                  slider.large.id ? "tag-success" : "tag-danger"
+                                )}
+                              >
+                                LG
+                              </span>
+                              <span
+                                className={clsx(
+                                  "tag ",
+                                  slider.xlarge.id
+                                    ? "tag-success"
+                                    : "tag-danger"
+                                )}
+                              >
+                                XL
+                              </span>
+                            </td>
+                            <td>{statusSliderFa[slider?.status]?.name_fa}</td>
+                            <td>
+                              <Link
+                                target="_blank"
+                                href={`/app-management/main/${slider?.id}`}
+                              >
+                                <span className="tag cursor-pointer text-blue-500">
+                                  {t("common:edit")}
+                                </span>
+                              </Link>
+                              /
+                              <span
+                                className="tag cursor-pointer text-error"
+                                onClick={(e) => {
+                                  e.stopPropagation()
+                                  console.log(slider.id)
+
+                                  setDeleteModalOpen(true)
+                                  setSliderToDelete(slider as Banner)
+                                }}
+                              >
+                                {t("common:delete")}
+                              </span>
+                            </td>
+                          </tr>
+                        )
+                    )}
+                  </tbody>
+                </table>
+              </div>
+              {/* <Pagination
               total={sliders?.data?.getBanners?.length?? 0}
               page={currentPage}
               onChange={(page) => {
                 setCurrentPage(page)
               }}
             /> */}
-          </>
-        )}
-      </CardContainer>
-    </div>
+            </>
+          )}
+        </CardContainer>
+      </div>
+    </>
   )
 }
 
