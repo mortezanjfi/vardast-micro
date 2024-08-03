3184dcb86c3070f0a3942cc2466c87ab934bf136
morteza najafi <mortezanajafiofficial@gmail.com>
2024-08-03 16:44:46 +0330

    add locales

---
+++ create-locale.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+
+const readAllFile = () => {
+  const dirMain = "./apps";
+  const readDirectoryMain = fs.readdirSync(dirMain);
+
+  readDirectoryMain.forEach((dirNext) => {
+    if (fs.lstatSync(dirMain + "/" + dirNext).isDirectory() && dirNext) {
+      fs.cpSync("./configs/locales", dirMain + "/" + dirNext + "/locales", {
+        recursive: true,
+      });
+    }
+  });
+};
+
+readAllFile();
