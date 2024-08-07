a042d45dc6e0a0dd610f044424a5b6f8c2eac0d4
mina ghoreshi <goreishi45@gmail.com>
2024-08-07 09:17:08 +0330

    fix: fix proflie redirect to loging page

--- apps/vardast-client/src/app/(client)/profile/components/ProfilePageIndex.tsx
+++ apps/vardast-client/src/app/(client)/profile/components/ProfilePageIndex.tsx
@@ -1,11 +1,20 @@
 /* eslint-disable no-unused-vars */
 "use client"
 
+import { redirect, usePathname } from "next/navigation"
 import Navigation from "@vardast/component/Navigation"
 import { SidebarProfile } from "@vardast/component/Sidebar"
+import paths from "@vardast/lib/paths"
 import sidebar_options from "@vardast/lib/sidebar_options"
+import { useSession } from "next-auth/react"
 
 const ProfilePageIndex = () => {
+  const { data: session, status } = useSession()
+
+  const pathname = usePathname()
+  if (!session?.profile || status === "unauthenticated")
+    redirect(`${paths.signin}?ru=${pathname}`)
+
   return (
     <div className="app-navigation">
       <div className="app-navigation-container">
