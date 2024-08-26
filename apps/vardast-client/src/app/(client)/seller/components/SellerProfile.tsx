3a7506c749948dd34a43dd6dccc3d3b12b6876e9
morteza najafi <mortezanajafiofficial@gmail.com>
2024-08-26 14:54:31 +0330

    merge mina-new

--- apps/vardast-client/src/app/(client)/seller/components/SellerProfile.tsx
+++ apps/vardast-client/src/app/(client)/seller/components/SellerProfile.tsx
@@ -18,17 +18,17 @@ import { getSellerQueryFn } from "@vardast/query/queryFns/sellerQueryFns"
 import { SegmentTabTitle } from "@vardast/ui/segment"
 import { useSetAtom } from "jotai"
 
+import ProductsTab from "@/app/(client)/brand/components/ProductsTab"
+
 import BrandOrSellersTab from "../../brand/components/BrandOrSellersTab"
 import { IBrandOrSellerProfile } from "../../brand/components/BrandProfile"
 import CategoriesTab from "../../brand/components/CategoryTab"
-import ProductsTab from "../../brand/components/ProductsTab"
 
 export enum SellerProfileTabEnum {
-   
   PRODUCT = "PRODUCT",
-   
+
   CATEGORY = "CATEGORY",
-   
+
   BRAND = "BRAND"
 }
 
